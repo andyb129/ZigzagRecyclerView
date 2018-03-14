1f251fa8f156c618869a9a9f9e9e2d2e7e0df2a2
Andy Barber <andy.barber@careflowconnect.com>
2018-03-14 22:30:16 +0000

    converted library to kotlin

---
+++ library/src/main/java/uk/co/barbuzz/zigzagrecyclerview/ZigzagImage.kt
@@ -0,0 +1,12 @@
+package uk.co.barbuzz.zigzagrecyclerview
+
+/**
+ * Created by andy.barber on 30/01/2018.
+ */
+
+interface ZigzagImage {
+
+    val zigzagImageUrl: String
+
+    val zigzagImageResourceId: Int
+}
